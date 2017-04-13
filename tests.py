91f4eb0cb64a5546ee2c43e1724947e46358bb2e
Henry Steinitz <Hjsteinitz@gmail.com>
2017-04-12 23:19:25 -0400

    Better example

--- tests.py
+++ tests.py
@@ -1,7 +1,10 @@
 from smooth import Flow, Map
 
-flow = Flow(['x'], ['y'])
+flow = Flow(['x', 'h', 'b'], ['y'])
 flow.connect_variable('x', [], ['f'])
-flow.connect_variable('y', ['f'], [])
-flow.connect_map('f', lambda x: [3*x], ['x'], ['y'])
-print(flow.compute({'x': 2}))
+flow.connect_variable('x', [], ['h'])
+flow.connect_variable('y', ['g'], [])
+flow.connect_variable('a', ['f'], ['g'])
+flow.connect_map('f', lambda x,h: [x*h], ['x', 'h'], ['a'])
+flow.connect_map('g', lambda a,b: [a-b], ['a', 'b'], ['y'])
+print(flow.compute({'x': 2, 'h': 8, 'b': 3}))
